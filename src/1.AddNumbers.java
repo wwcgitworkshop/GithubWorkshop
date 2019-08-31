095154428628670f97249bc71f19f08afb7836e3
arusheeg <agarg@yourmechanic.com>
2019-08-31 12:22:13 -0700

    Added a java file to add two numbers

---
+++ src/1.AddNumbers.java
@@ -0,0 +1,19 @@
+/*
+ * Add two numbers a and b
+ */
+class AddNumbers {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main (String[] args){
+      // Test case 1
+      int a = 3;
+      int b = 4;
+      int c  = addNumbers(a, b);
+      System.out.println(c);
+
+    }
+}
+
+
